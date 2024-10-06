ad0a3d66f169545f9829c936d3aafa1ae61ea5a7
blast59 <shashankpoojary59@gmail.com>
2024-10-06 17:40:54 +0530

    instead of overthinking

---
+++ QuizApp/Data Server/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuizApp.Models;
+
+namespace QuizApp.Data_Server
+{
+    public class AppDbContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+        public AppDbContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            // connect to postgres with connection string from app settings
+            options.UseNpgsql(Configuration.GetConnectionString("WebApiDatabase"));
+        }
+        public DbSet<Question> Question { get; set; }
+    }
+}
---
+++ QuizApp/Models/Question.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata.Ecma335;
+
+namespace QuizApp.Models
+{
+    public class Question
+    {
+        [Key]
+        public int QuestionId { get; set; }
+        public int QuizId { get; set; }
+        [Required]
+        public string QuestionText { get; set; }
+        //public string OptionA { get; set; }
+        //public string OptionB { get; set; }
+        //public string OptionC { get; set; }
+        //public string OptionD { get; set; }
+        //public string CorrectOption { get; set; }
+        //public int TimeLimitInSeconds { get; set; }
+        //public int Difficulty { get; set; }
+
+        //public int CreatedBy { get; set; }
+        //public int CreatedAt { get; set; }
+    }
+}
