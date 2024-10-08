b69e1cce73d36364e5667b0a40f669a8984dde2e
Durgaprasad <durgaprasad.m@secon.in>
2024-10-08 17:53:39 +0530

    Done for the day(08-10-24)

--- QuizApp/Controllers/HomeController.cs
+++ QuizApp/Controllers/HomeController.cs
@@ -14,6 +14,7 @@ namespace QuizApp.Controllers
             _logger = logger;
         }
 
+
         public IActionResult Index()
         {
             return View();
---
+++ QuizApp/Controllers/QuestionController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QuizApp.Data_Server;
+using QuizApp.Models;
+using System.Runtime.CompilerServices;
+using System.Security;
+namespace QuizApp.Controllers
+{
+    public class QuestionController : Controller
+    {
+        private readonly AppDbContext _db;
+        public QuestionController(AppDbContext db)
+        {
+            _db = db;
+        }
+        
+
+        public IActionResult Create(int? id)
+        {
+            TempData["quizId"] = id; 
+            Quiz obj = _db.Quiz.Find(id);
+            return View(obj);
+        }
+        // POST: Question/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Question question)
+        {
+            if (ModelState.IsValid)
+            {
+                // Add the question to the database
+                _db.Question.Add(question);
+                _db.SaveChanges();
+
+                return RedirectToAction("Index", "Quiz"); // Redirect to the quiz list or another page
+            }
+
+            return View(question);
+        }
+
+
+    }
+}
